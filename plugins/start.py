fc2927ad34aa7f43cab88d6d3bb928689bb11d1d
RimaruSESNEI <167967594+RimaruSESNEI@users.noreply.github.com>
2024-04-25 13:03:16 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -102,7 +102,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 ᴘʀᴇᴍɪᴜᴍ', url='https://t.me/LUFFY1JOYBOY')
+                    InlineKeyboardButton('🍁 ᴄʀᴇᴀᴛᴏʀ', url='https://t.me/LUFFY1JOYBOY')
                 ]
             ]
         )
